e3f8c9a925c08121f6975b350a95cf83a10cd956
dnmarch <39606775+dnmarch@users.noreply.github.com>
2020-11-01 17:20:41 -0800

    Add files via upload

--- ProtoNet.py
+++ ProtoNet.py
@@ -58,3 +58,31 @@ def ProtoLoss(x_latent, q_latent, labels_onehot, num_classes, num_support, num_q
     acc = tf.reduce_mean(num_correct)
     #############################
     return ce_loss, acc
+
+def proto_net_train_step(model, optim, x, q, labels_ph):
+    num_classes, num_support, im_height, im_width, channels = x.shape
+    num_queries = q.shape[1]
+    x = tf.reshape(x, [-1, im_height, im_width, channels])
+    q = tf.reshape(q, [-1, im_height, im_width, channels])
+
+    with tf.GradientTape() as tape:
+        x_latent = model(x)
+        q_latent = model(q)
+        ce_loss, acc = ProtoLoss(x_latent, q_latent, labels_ph, num_classes, num_support, num_queries)
+
+    gradients = tape.gradient(ce_loss, model.trainable_variables)
+    optim.apply_gradients(zip(gradients, model.trainable_variables))
+    return ce_loss, acc
+
+
+def proto_net_eval(model, x, q, labels_ph):
+    num_classes, num_support, im_height, im_width, channels = x.shape
+    num_queries = q.shape[1]
+    x = tf.reshape(x, [-1, im_height, im_width, channels])
+    q = tf.reshape(q, [-1, im_height, im_width, channels])
+
+    x_latent = model(x)
+    q_latent = model(q)
+    ce_loss, acc = ProtoLoss(x_latent, q_latent, labels_ph, num_classes, num_support, num_queries)
+
+    return ce_loss, acc
